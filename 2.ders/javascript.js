891c8da0926e9b5c2fffb61ae05fa457f99a47ae
ramazangalip <ramazansaidgalip@gmail.com>
2023-07-12 22:45:13 +0300

    12.07.2023

---
+++ 2.ders/javascript.js
@@ -0,0 +1,190 @@
+// js function
+
+// function   myFunction(parameter1,parameter2){
+//     //kodların çalıştığı yer
+// }
+
+// myFunction(argument1,argument2)
+
+// let b = function () {}
+
+
+//  function carpma(ç1,ç2){
+//      return ç1*ç2;
+
+//  }
+
+// console.log(carpma(8,9))
+
+
+// function toCelsius(fahrenheit){
+//     return(5/9) * (fahrenheit - 32)
+// }
+
+// console.log("Celcius Sıcaklıgı : ",toCelsius(64))
+
+// function myFunction() {
+//     let carName  = "Dacia"
+//     console.log(carName)
+// }
+
+// myFunction()
+
+// // js objects
+
+// const person = {
+//     isim:"Ramazan", Soyisim:"Galip",Yaş:"16",isimTamamı:function myFunction(){
+//         return this.isim + " " +this.Soyisim
+//     }
+// }
+
+// console.log(person.isim,person.Soyisim)
+// console.log(person.isimTamamı())
+
+// //fonksiyonu objede tanımlama şekilleri
+// // fullName: function()
+// // myFunction()
+
+// let x  = new Date();
+
+
+// console.log(x)
+
+// js string
+
+// escape character
+
+// let text = "onun adına 'ramazan'  denirdi" 
+
+// console.log(text)
+
+// let text2 = "tab boşluk bırakmak için \t isareti kullanılabilir eğer alt satıra geçiceksem \n işareti kullanılır"
+
+// console.log(text2)
+
+//string methods
+
+// let text ="kjdjıoeqhdıhfhwffjewıpjfwr"
+// let lenght = text.length;
+// console.log(lenght)
+
+
+// let text = "Apple ,Banana,Kiwi"
+// let part = text.slice(7,13)
+// console.log(part)
+
+// let part2 = text.slice(7)
+// console.log(part2)
+
+// let part3 =  text.slice(-12)
+
+// let part4 = text.substr(7,5)
+
+// console.log(part4)
+
+// let part5 = text.substring(-8,-2)
+// console.log(part5)
+
+// let text = "Seni Seviyorum"
+
+// let newText = text.replace("seviyorum","sevmiyorum")
+// console.log(newText)
+
+// let text = "Seni Seviyorum"
+// let newText = text.replace("Seviyorum","sevmiyorum")
+// console.log(newText)
+
+// let text1  = "Hello World!"
+
+// let text2 = text1.toUpperCase();
+
+// console.log(text2)
+
+// let text3 = text1.toLowerCase();
+// console.log(text3)
+
+// let text1 = "hello"
+// let text2 = "world"
+// let text3 = "!"
+// let text4 = text1.concat(" ",text2," ",text3)
+// console.log(text4)
+
+
+// let text = "567"
+// let withPadStart = text.padStart(5,"0")
+// console.log(withPadStart)
+
+
+// let withPadEnd  = withPadStart.padEnd(10,"x")
+
+// console.log(withPadEnd)
+
+// let text = "HELLO WORLD!"
+
+// let charAt = text.charAt(1)
+// console.log(charAt)
+
+// let text = "elma,şeftali,kayısı"
+// let splitText  = text.split(",")
+// console.log(splitText)
+
+
+// let text = "Please locate where 'locate' occurs!"
+// let index = text.indexOf("locate")
+// console.log(index)
+
+// let index2 = text.lastIndexOf("locate")
+// console.log(index2)
+
+// let index3 = text.indexOf("locate",15)
+
+// console.log(index3)
+
+
+// let text = "Hello World!"
+
+// let textIncludes = text.includes("World")
+// console.log(textIncludes)
+
+// let text = "Hello World!"
+
+// let textStartsWith = text.startsWith("Hello");
+// console.log(textStartsWith)
+// let textEndSwith = text.endsWith("universe")
+// console.log(textEndSwith)
+
+// js template literals 
+
+// let text = 'ramazan galip'
+
+// console.log(text)
+
+// js numbers 
+
+// let x = 3.24
+
+// let y  = 1234e5
+
+// let x = "199"
+// let y = "19"
+// console.log(x/y)
+
+// let x  = 10
+// let y  = x.toString()
+
+// let z = Number(y)
+// console.log(typeof z)
+
+// console.log(Number(true))
+// console.log(Number(false))
+// console.log(Number(" 10"))
+// console.log(Number("2"))
+// console.log(Number("3"))
+// console.log(Number("34.40"))
+// console.log(Number("23"))
+
+// console.log(parseInt("-10"))
+// console.log(parseInt("-10.33"))
+// console.log(parseInt("0010"))
+// console.log(parseInt("10.3"))
+// console.log(parseInt())
